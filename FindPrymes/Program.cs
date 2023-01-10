f1dc22a52ac43418e15c035f6d52e1a374c49c75
olegshok <pastushok91@gmail.com>
2023-01-10 23:09:00 +0200

    Change Console.Writeline and use {} instead "+ _+"

--- FindPrymes/Program.cs
+++ FindPrymes/Program.cs
@@ -6,7 +6,7 @@ using System.Collections.Generic;
 int n = 100;
 var primesParallelForEach = await PrimeFinderUsingParallelForEach.FindPrimesAsyncUsingParallelForEach(n);
 
-Console.WriteLine("Prime numbers using realization Parallel.ForEach from 1 to " + n + ":");
+Console.WriteLine($"Prime numbers using realization Parallel.ForEach from 1 to {n}:");
 foreach (int primeNumberParallelForEach in primesParallelForEach)
 {
     Console.WriteLine(primeNumberParallelForEach);
@@ -16,7 +16,7 @@ foreach (int primeNumberParallelForEach in primesParallelForEach)
 int m = 100;
 var primesTask = await PrimeFinderUsingTask.FindPrimesAsyncUsingTask(m);
 
-Console.WriteLine("Prime numbers using realization Task from 1 to " + m + ":");
+Console.WriteLine($"Prime numbers using realization Task from 1 to {m}:");
 foreach (int primeNumberTask in primesTask)
 {
     Console.WriteLine(primeNumberTask);
@@ -26,7 +26,7 @@ foreach (int primeNumberTask in primesTask)
 int k = 100;
 var primesThread = PrimeFinderUsingThread.FindPrimesAsyncUsingThread(k);
 
-Console.WriteLine("Prime numbers using realization Thread from 1 to " + k + ":");
+Console.WriteLine($"Prime numbers using realization Thread from 1 to {k}:");
 foreach (int primeNumberThread in primesThread)
 {
     Console.WriteLine(primeNumberThread);
@@ -36,7 +36,7 @@ foreach (int primeNumberThread in primesThread)
 int l = 100;
 var primesEratosthenes = await PrimeFinderUsingEratosthenes.FindPrimesAsyncUsingEratosthenes(l);
 
-Console.WriteLine("Prime numbers using realization The Sieve of Eratosthenes from 1 to " + l + ":");
+Console.WriteLine($"Prime numbers using realization The Sieve of Eratosthenes from 1 to {l}:");
 foreach (int primeNumberEratosthenes in primesEratosthenes)
 {
     Console.WriteLine(primeNumberEratosthenes);
