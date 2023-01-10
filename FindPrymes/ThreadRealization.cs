bb9a671103d7eeaaf6f557410056914c3d5be5ab
olegshok <pastushok91@gmail.com>
2023-01-10 21:44:54 +0200

    Changed ThreadRealization.cs: changed method Thread - each number is checked in a separate thread.

--- FindPrymes/ThreadRealization.cs
+++ FindPrymes/ThreadRealization.cs
@@ -6,20 +6,36 @@ public static class PrimeFinderUsingThread
     public static ConcurrentBag<int> FindPrimesAsyncUsingThread(int k)
     {
         var bag = new ConcurrentBag<int>();
-        var thread = new Thread(() =>
+        var threads = new List<Thread>();
+        var numbersPerThread = (k - 2) / Environment.ProcessorCount;
+        for (int i = 2; i <= k; i += numbersPerThread)
         {
-            for (int i = 2; i <= k; i++)
+            int start = i;
+            int end = i + numbersPerThread - 1;
+            if (end > k) end = k;
+
+            var thread = new Thread(() =>
             {
-                if (IsPrime(i))
+                for (int j = start; j <= end; j++)
                 {
-                    bag.Add(i);
+                    if (IsPrime(j))
+                    {
+                        bag.Add(j);
+                    }
                 }
-            }
-        });
-        thread.Start();
-        thread.Join();
+            });
+            thread.Start();
+            threads.Add(thread);
+        }
+
+        foreach (var thread in threads)
+        {
+            thread.Join();
+        }
+
         return bag;
     }
+
 
     private static bool IsPrime(int number)
     {
